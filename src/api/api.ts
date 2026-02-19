a087df1bdc3287f15b66554e0d99d0205aa8eb86
Sohidul-Islam <sishufol.sim@gmail.com>
2026-02-19 06:37:37 +0600

    feat: create API service with centralized endpoints for product, authentication, order, payroll, and notifications.

--- src/api/api.ts
+++ src/api/api.ts
@@ -6,8 +6,8 @@ import {
   Product,
 } from "@/types/ProductType";
 
-// export const BASE_URL = "https://glorypos.com/api/api";
-export const BASE_URL = "http://localhost:3000/api";
+export const BASE_URL = "https://glorypos.com/api/api";
+// export const BASE_URL = "http://localhost:3000/api";
 
 export const REGISTER_URL = `${BASE_URL}/register`;
 export const LOGIN_URL = `${BASE_URL}/login`;
