41de720021f9908d4a7244a7d514b919f3b86afc
Sohidul-Islam <sishufol.sim@gmail.com>
2026-03-06 01:08:30 +0600

    feat: create InputWithIcon component with support for icons, password toggling, and labels.

--- src/components/InputWithIcon.tsx
+++ src/components/InputWithIcon.tsx
@@ -63,7 +63,7 @@ const InputWithIcon: React.FC<InputWithIconProps> = ({
       <div className="relative">
         {/* ICON */}
         {Icon && (
-          <div className="absolute z-30 border-r border-gray-200 inset-y-0 left-0 px-3 flex items-center pointer-events-none">
+          <div className="absolute z-50 border-r border-gray-200 inset-y-0 left-0 px-3 flex items-center pointer-events-none">
             {typeof Icon === "function" ? (
               <Icon className="w-[16px] h-[16px] text-gray-400" />
             ) : (
@@ -79,9 +79,8 @@ const InputWithIcon: React.FC<InputWithIconProps> = ({
             name={name}
             type={type === "password" && showPassword ? "text" : type}
             required={required}
-            className={`appearance-none no-spinner disabled:bg-gray-50 disabled:border-gray-200 disabled:text-gray-400 disabled:placeholder-gray-300 disabled:cursor-not-allowed bg-white placeholder:text-gray-400 text-gray-700 relative block w-full ${
-              Icon ? "pl-12" : "pl-2"
-            } pr-10 py-2 border border-gray-200 rounded-[4px] focus:outline-none focus:ring-brand-primary focus:border-brand-primary focus:z-10 sm:text-sm ${className}`}
+            className={`appearance-none no-spinner disabled:bg-gray-50 disabled:border-gray-200 disabled:text-gray-400 disabled:placeholder-gray-300 disabled:cursor-not-allowed bg-white placeholder:text-gray-400 text-gray-700 relative block w-full ${Icon ? "pl-12" : "pl-2"
+              } pr-10 py-2 border border-gray-200 rounded-[4px] focus:outline-none focus:ring-brand-primary focus:border-brand-primary sm:text-sm ${className}`}
             placeholder={placeholder}
             value={inputValue}
             defaultValue={defaultValue}
@@ -109,7 +108,7 @@ const InputWithIcon: React.FC<InputWithIconProps> = ({
           <button
             type="button"
             onClick={() => setShowPassword((p) => !p)}
-            className="absolute inset-y-0 right-0 px-3 flex items-center cursor-pointer text-gray-400 hover:text-gray-600"
+            className="absolute z-50 inset-y-0 right-0 px-3 flex items-center cursor-pointer text-gray-400 hover:text-gray-600"
           >
             {showPassword ? (
               <FaEyeSlash className="w-[16px] h-[16px]" />
